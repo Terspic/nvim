7f9a7c0ec3ab863cf78d959597aa7f2bf0708464
Julien <julien.flot08@gmail.com>
2023-03-07 21:39:27 +0100

    suppression de fichier inutile + correction sur les bindings de rust

--- lua/langages/rust.lua
+++ lua/langages/rust.lua
@@ -7,11 +7,11 @@ vim.api.nvim_create_autocmd(
 		group = group,
 		pattern = "*.rs",
 		callback = function()
-			vim.api.nvim_set_keymap("n", "<leader>rr", ":Cargo run<cr>", { silent = true })
-			vim.api.nvim_set_keymap("n", "<leader>rR", ":Cargo run --release<cr>", { silent = true })
-			vim.api.nvim_set_keymap("n", "<leader>rb", ":Cargo build<cr>", { silent = true })
-			vim.api.nvim_set_keymap("n", "<leader>rB", ":Cargo build --release<cr>", { silent = true })
-			vim.api.nvim_set_keymap("n", "<leader>rc", ":Cargo clean<cr>", { silent = true })
+			vim.keymap.set("n", "<leader>rr", ":Cargo run<cr>", { silent = true })
+			vim.keymap.set("n", "<leader>rR", ":Cargo run --release<cr>", { silent = true })
+			vim.keymap.set("n", "<leader>rb", ":Cargo build<cr>", { silent = true })
+			vim.keymap.set("n", "<leader>rB", ":Cargo build --release<cr>", { silent = true })
+			vim.keymap.set("n", "<leader>rc", ":Cargo clean<cr>", { silent = true })
 		end
 	}
 )
